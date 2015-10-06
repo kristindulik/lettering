f3998f377fdd7acf7a9f025bcb1b9c03f69b4b52
Kristin Dulik <kristindulik@gmail.com>
2015-10-06 14:34:46 -0700

    Letting Example in Class

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// My Scripts
+
+
+// Default Show No Name
+// Get & Store First Name
+// var firstName = prompt ('What is your first name?');
+// console.log(firstName);
+
+// Get & Store Last Name
+// var lastName = prompt ('What is your last name?');
+// console.log(lastName);
+
+
+// Display First & Last Name
+// $('.first').text(firstName);
+// $('.last').text(lastName);
+
+//Turn on Lettering
+$('h1').lettering();
